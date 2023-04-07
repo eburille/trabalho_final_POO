a01fd4b3b4fd6a82eafa8c9afc4e8d90b5575148
tiagorodrisi <112359948+tiagorodrisi@users.noreply.github.com>
2023-04-07 13:55:49 -0300

    Update Lampada.java

--- Lampada.java
+++ Lampada.java
@@ -1,6 +1,7 @@
 public class Lampada extends DispositivoInteligente {
     public Lampada(String dispositivo) {
         super(dispositivo);
+        intensidade = 50;
     }
     
     public void ligar() {
@@ -12,4 +13,18 @@ public class Lampada extends DispositivoInteligente {
         setEstado(false);
         System.out.println(getDispositivo() + " desligada.");
     }
+    
+    public void setIntensidade(int novaIntensidade) {
+        if (novaIntensidade >= 0 && novaIntensidade <= 100) {
+            intensidade = novaIntensidade;
+            System.out.println(getDispositivo() + " intensidade ajustada para " + intensidade + ".");
+        } else {
+            System.out.println("Intensidade inválida. Use um valor entre 0 e 100.");
+        }
+    }
+
+    public int getIntensidade() {
+        return intensidade;
+    }
+
 }
