d270ad98e5866acc23e6a3ac2fd086da58305906
tiagorodrisi <112359948+tiagorodrisi@users.noreply.github.com>
2023-04-07 13:44:24 -0300

    Update ArCondicionado.java

--- ArCondicionado.java
+++ ArCondicionado.java
@@ -14,4 +14,14 @@ public class ArCondicionado extends DispositivoInteligente {
         this.temperatura = temperatura;
         System.out.println("A temperatura do " + getDispositivo() + " agora é " + temperatura + " graus Celsius.");
     }
+    
+    public void ligar() {
+        setEstado(true);
+        System.out.println(getDispositivo() + " ligado.");
+    }
+    
+    public void desligar() {
+        setEstado(false);
+        System.out.println(getDispositivo() + " desligado.");
+    }
 }
